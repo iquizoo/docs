4d64c7465a79e324f5ff4f343e9bc5474f630690
Zhang Liang <psychelzh@outlook.com>
2018-04-04 15:27:01 +0800

    Rename output sample bank

--- calculator/itembank.R
+++ calculator/itembank.R
@@ -51,6 +51,6 @@ itembank <- expand.grid(left = 10:99, right = 10:99) %>%
   filter(type == "add" | (type == "minus" & correct > 0)) %>%
   mutate(title = paste(left, recode(type, add = "+", minus = "-"), right)) %>%
   select(title, type, difficulty, correct, opt1, opt2, opt3)
-write_csv(itembank, here("calculator", "itembank.csv"))
+write_csv(itembank, here("calculator", "calculator.csv"))
 set.seed(1)
-write_csv(sample_n(itembank, 10), here("calculator", "stimuli.csv"))
+write_csv(sample_n(itembank, 10), here("calculator", "itembank_sample.csv"))
